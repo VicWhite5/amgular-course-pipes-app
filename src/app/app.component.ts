b90c81a9f43b551b59584a5384aebfab744af896
Victor White <victorblanco281201@gmail.com>
2024-08-02 22:43:14 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipes-app';
+  public title = 'pipes-app';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
 }
---
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styles: ``,
+})
+export class MenuBarComponent implements OnInit {
+  public menuItems: MenuItem[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            // routerLink: '/'
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            // routerLink: 'numeros'
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+            // routerLink: 'no-comunes'
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        // routerLink: 'ordenar'
+        items: [
+          {
+            label: 'Ordenar',
+            icon: 'pi pi-sort-alpha-down',
+            // routerLink: 'ordenar'
+          },
+        ],
+      },
+    ];
+  }
+}
